e5a9839a181d4f7ace0ad3f22cf3e0a3b02b0bf0
Kelita Lamar <kelitasukita@gmail.com>
2023-02-17 18:32:42 +0100

    Create basic structures from HTML

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,31 @@
+import './styles/global.scss';
+import './styles/desktopVersion.scss';
+import desktopParfum from './assets/images/image-product-desktop.jpg';
 
 function App() {
   return (
-  <div>
-    <h1>Parfum</h1>
-  </div>
+    <>
+      <div>
+        <img src={desktopParfum} alt="" />
+      </div>
+      <div>
+        <h6>PERFUME</h6>
+        <h1>Gabrielle Essence Eau De Parfum</h1>
+
+        <p>
+          A floral, solar and voluptuous interpretation composed bu Oliver Polge, Pergfumer-Creator 
+          for the House of CHANEL.
+        </p>
+
+        <strong>$149.99</strong>
+        <span>$169.99</span>
+
+        <div>
+          <button>Add to Cart</button>
+        </div>
+      </div>  
+    </>
+  
   )
 }
 
